ce596f0c51a9dc41aacb49b9bbb218b818648a4a
hung.dao11 <hung.dao1@mservice.com.vn>
2022-12-14 22:30:59 +0700

    no message

--- app/src/main/java/com/example/learnarchitecture/Application.kt
+++ app/src/main/java/com/example/learnarchitecture/Application.kt
@@ -1,6 +1,8 @@
 package com.example.learnarchitecture
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class Application: Application() {
 
     override fun onCreate() {
